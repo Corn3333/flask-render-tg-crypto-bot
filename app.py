740315452968804b634bf0dde949801e3cdae549
doonay333331 <doonay333331@gmail.com>
2023-05-22 20:33:02 +0300

    1

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import os
 from flask import Flask, request, Response
-from telegram import Update, Bot
-from telegram.ext import Updater, CommandHandler, CallbackContext
+# from telegram import Update, Bot
+# from telegram.ext import Updater, CommandHandler, CallbackContext
+import requests
 
 app = Flask(__name__)
 
 # Set up Telegram bot API
 TELEGRAM_API_TOKEN = os.environ['BOT_TOKEN']
-bot = Bot(TELEGRAM_API_TOKEN)
+# bot = Bot(TELEGRAM_API_TOKEN)
 user_chat_id = os.environ['CHANNEL_ID']
 
 @app.route('/')
@@ -16,43 +17,56 @@ def hello():
 
 @app.route('/notify', methods=['POST','GET'])
 def notify():
-  logs = request.json
-  print(logs)
-  bot.send_message(chat_id=user_chat_id, text=logs)
-  return Response(status=200)
-  if (len(logs) == 0):
-    print("Empty logs array received, skipping")
-  else:    
+    logs = request.json
+    
+    message = logs
+    url = f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage?chat_id={user_chat_id}&text={message}"
+    requests.get(url, stream=True)
+    return Response(status=200)
+  
+  
+  
+
+
+# @app.route('/notify', methods=['POST','GET'])
+# def notify():
+#   logs = request.json
+#   print(logs)
+#   bot.send_message(chat_id=user_chat_id, text=logs)
+#   return Response(status=200)
+#   if (len(logs) == 0):
+#     print("Empty logs array received, skipping")
+#   else:    
       
-      print(logs)
+#       print(logs)
       
-      category = ""
-      try:
-         category = logs['event']['activity'][0]['category']
-      except:
-         print("category not defined")
+#       category = ""
+#       try:
+#          category = logs['event']['activity'][0]['category']
+#       except:
+#          print("category not defined")
       
-      if logs['webhookId']==os.environ['ALCHEMY_KEY'] and category == 'token':
-        # extract the necessary information
-        txhash = from_address = "["+str(logs['event']['activity'][0]['hash'])+"](https://etherscan.io/tx/"+str(logs['event']['activity'][0]['hash'])+")"
+#       if logs['webhookId']==os.environ['ALCHEMY_KEY'] and category == 'token':
+#         # extract the necessary information
+#         txhash = from_address = "["+str(logs['event']['activity'][0]['hash'])+"](https://etherscan.io/tx/"+str(logs['event']['activity'][0]['hash'])+")"
          
-        from_address = "["+str(logs['event']['activity'][0]['fromAddress'])+"](https://etherscan.io/address/"+str(logs['event']['activity'][0]['fromAddress'])+"#tokentxns)"
-        to_address = "["+str(logs['event']['activity'][0]['toAddress'])+"](https://etherscan.io/address/"+str(logs['event']['activity'][0]['toAddress'])+"#tokentxns)"
+#         from_address = "["+str(logs['event']['activity'][0]['fromAddress'])+"](https://etherscan.io/address/"+str(logs['event']['activity'][0]['fromAddress'])+"#tokentxns)"
+#         to_address = "["+str(logs['event']['activity'][0]['toAddress'])+"](https://etherscan.io/address/"+str(logs['event']['activity'][0]['toAddress'])+"#tokentxns)"
         
-        token_symbol = logs['event']['activity'][0]['asset']
-        token_address = "["+str(logs['event']['activity'][0]['rawContract']['address'])+"](https://etherscan.io/address/"+str(logs['event']['activity'][0]['rawContract']['address'])+")"
+#         token_symbol = logs['event']['activity'][0]['asset']
+#         token_address = "["+str(logs['event']['activity'][0]['rawContract']['address'])+"](https://etherscan.io/address/"+str(logs['event']['activity'][0]['rawContract']['address'])+")"
         
-        value = str(round(logs['event']['activity'][0]['value']))
+#         value = str(round(logs['event']['activity'][0]['value']))
 
-        # create the text string
-        message = f'*Token transfer:*\n{txhash}\nfrom {from_address} \nto {to_address}: \nvalue: {value} *{token_symbol}* {token_address}'
-        bot.send_message(chat_id=user_chat_id, text=message, parse_mode='MarkdownV2')
+#         # create the text string
+#         message = f'*Token transfer:*\n{txhash}\nfrom {from_address} \nto {to_address}: \nvalue: {value} *{token_symbol}* {token_address}'
+#         bot.send_message(chat_id=user_chat_id, text=message, parse_mode='MarkdownV2')
       
-  return Response(status=200)
+#   return Response(status=200)
 
-updater = Updater(TELEGRAM_API_TOKEN)
-# Start the bot
-updater.start_polling()
+# updater = Updater(TELEGRAM_API_TOKEN)
+# # Start the bot
+# updater.start_polling()
 
 if __name__ == '__main__':
     app.run()
