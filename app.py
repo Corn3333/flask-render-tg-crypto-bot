be85716a862f74fb5eaef3204612ad87f50c9347
doonay333331 <doonay333331@gmail.com>
2023-05-23 20:45:14 +0300

    1.1

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 # Set up Telegram bot API
 TELEGRAM_API_TOKEN = os.environ['BOT_TOKEN']
-
+variables = {'fromAddress' : '\nfrom: ', 'toAddress' : '\nto: ', 'value' : '\nsent: ', 'asset' : '', 'external' : '\ncategory: ', 'hash': '\nhash: '}
 user_chat_id = os.environ['CHANNEL_ID']
 
 @app.route('/')
@@ -18,21 +18,28 @@ def hello():
 @app.route('/notify', methods=['POST','GET'])
 def notify():
     logs = request.json
-    network = logs['event']['network']
-    from_address = logs['event']['activity'][0]['fromAddress']
-    to_address = logs['event']['activity'][0]['toAddress']
-    value = logs['event']['activity'][0]['value']
-    assets = logs['event']['activity'][0]['asset']
-    category = logs['event']['activity'][0]['external']
-    tx_hash = logs['event']['activity'][0]['hash']
+    # network = logs['event']['network']
+    # from_address = logs['event']['activity'][0]['fromAddress']
+    # to_address = logs['event']['activity'][0]['toAddress']
+    # value = logs['event']['activity'][0]['value']
+    # assets = logs['event']['activity'][0]['asset']
+    # category = logs['event']['activity'][0]['external']
+    # tx_hash = logs['event']['activity'][0]['hash']
     
-    message = f'''{network}
-    from: {from_address}
-    to: {to_address}
-    sent: {value} {assets}
-    category: {category}
-    hash: {tx_hash}
-    '''
+    message = logs['event']['network']
+    for i in variables:
+        res = logs['event']['activity'][0].get(i)
+        if res: message += variables[i] + res
+            
+    
+    
+    # message = f'''{network}
+    # from: {from_address}
+    # to: {to_address}
+    # sent: {value} {assets}
+    # category: {category}
+    # hash: {tx_hash}
+    # '''
     url = f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage?chat_id={user_chat_id}&text={message}"
     requests.get(url, stream=True)
     return Response(status=200)
