548f2ba3a8f4ea3feb81ba6cd85c0bf9c481fee1
doonay333331 <doonay333331@gmail.com>
2023-05-24 13:10:44 +0300

    1.6

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ app = Flask(__name__)
 TELEGRAM_API_TOKEN = os.environ['BOT_TOKEN']
 variables = {'fromAddress' : '\nfrom:', 'toAddress' : '\nto:', 'value' : '\nsent: ', 'asset' : ' ', 'hash': '\nhash: '}
 user_chat_id = os.environ['CHANNEL_ID']
-NETWORK = {"ARB_MAINNET" : {"url_address" : "https://arbiscan.io/address/", "url_hash" : "https://arbiscan.io/tx/"}, "MATIC_MAINNET" : {"url_address" : "https://polygonscan.com/address/", "url_hash" : "https://polygonscan.com/tx/"}, "ETH_MAINNET": {"url_address" : "https://etherscan.io/address/", "url_hash" : "https://etherscan.io/tx/"}, "OPT_MAINNET": {"url_address" : "https://optimistic.etherscan.io/address/", "url_hash" : "https://optimistic.etherscan.io/tx/"}}
+NETWORK = {
+    "ARB_MAINNET" : {"name" : "ARBITRUM", "url_address" : "https://arbiscan.io/address/", "url_hash" : "https://arbiscan.io/tx/"}, 
+    "MATIC_MAINNET" : {"name" : "POLYGON", "url_address" : "https://polygonscan.com/address/", "url_hash" : "https://polygonscan.com/tx/"}, 
+    "ETH_MAINNET": {"name" : "ETH", "url_address" : "https://etherscan.io/address/", "url_hash" : "https://etherscan.io/tx/"}, 
+    "OPT_MAINNET": {"name" : "OPTIMISM", "url_address" : "https://optimistic.etherscan.io/address/", "url_hash" : "https://optimistic.etherscan.io/tx/"}}
 
 @app.route('/')
 def hello():
@@ -21,11 +25,10 @@ def notify():
     
     message = ""
     k = logs['event'].get('network')
-    if k: message += f"<b>{k}</b>"
+    if k: message += f"<b>{NETWORK[k]['name']}</b>"
     for i in variables:
         res = logs['event']['activity'][0].get(i)
         if res and (i == "fromAddress" or i == "toAddress"): 
-            # message += f"<b>{variables[i]}</b>" + f"<code>{str(res)}</code>"
             message += f"<i>{variables[i]}</i> <a href='{NETWORK[k]['url_address'] + str(res)}'>{str(res)[:6]}</a>"
         elif res and i == "hash":
             message += f"\n<a href='{NETWORK[k]['url_hash'] + str(res)}'>Tx hash</a>"
@@ -34,7 +37,7 @@ def notify():
     
     # message += f"\n\n{logs}"
 
-    url = f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage?chat_id={user_chat_id}&parse_mode=HTML&text={message}"
+    url = f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage?disable_web_page_preview=true&chat_id={user_chat_id}&parse_mode=HTML&text={message}"
     requests.get(url, stream=True)
     return Response(status=200)
   
