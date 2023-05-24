0fab5e0da7cd006fbd0d4cb49b3069ffa09657f0
doonay333331 <doonay333331@gmail.com>
2023-05-24 12:53:29 +0300

    1.5

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ app = Flask(__name__)
 
 # Set up Telegram bot API
 TELEGRAM_API_TOKEN = os.environ['BOT_TOKEN']
-variables = {'fromAddress' : '\nfrom: ', 'toAddress' : '\nto: ', 'value' : '\nsent: ', 'asset' : ' ', 'external' : '\ncategory: ', 'hash': '\nhash: '}
+variables = {'fromAddress' : '\nfrom:', 'toAddress' : '\nto:', 'value' : '\nsent: ', 'asset' : ' ', 'hash': '\nhash: '}
 user_chat_id = os.environ['CHANNEL_ID']
-
+NETWORK = {"ARB_MAINNET" : {"url_address" : "https://arbiscan.io/address/", "url_hash" : "https://arbiscan.io/tx/"}, "MATIC_MAINNET" : {"url_address" : "https://polygonscan.com/address/", "url_hash" : "https://polygonscan.com/tx/"}, "ETH_MAINNET": {"url_address" : "https://etherscan.io/address/", "url_hash" : "https://etherscan.io/tx/"}, "OPT_MAINNET": {"url_address" : "https://optimistic.etherscan.io/address/", "url_hash" : "https://optimistic.etherscan.io/tx/"}}
 
 @app.route('/')
 def hello():
     return 'Service for sending notifications to a telegram channel '
+
 
 @app.route('/notify', methods=['POST','GET'])
 def notify():
@@ -23,9 +24,15 @@ def notify():
     if k: message += f"<b>{k}</b>"
     for i in variables:
         res = logs['event']['activity'][0].get(i)
-        if res: message += f"<b>{variables[i]}</b>" + f"<code>{str(res)}</code>"
+        if res and (i == "fromAddress" or i == "toAddress"): 
+            # message += f"<b>{variables[i]}</b>" + f"<code>{str(res)}</code>"
+            message += f"<i>{variables[i]}</i> <a href='{NETWORK[k]['url_address'] + str(res)}'>{str(res)[:6]}</a>"
+        elif res and i == "hash":
+            message += f"\n<a href='{NETWORK[k]['url_hash'] + str(res)}'>Tx hash</a>"
+        elif res:
+            message += f"<i>{variables[i]}</i>" + f"<code>{str(res)}</code>"
     
-    message += f"\n\n{logs}"
+    # message += f"\n\n{logs}"
 
     url = f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage?chat_id={user_chat_id}&parse_mode=HTML&text={message}"
     requests.get(url, stream=True)
