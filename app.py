7628457495b71860ef863b235a85aac4e048e476
doonay333331 <doonay333331@gmail.com>
2023-05-24 12:17:59 +0300

    1.4

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import os
 from flask import Flask, request, Response
-# from telegram import Update, Bot
-# from telegram.ext import Updater, CommandHandler, CallbackContext
 import requests
 
 app = Flask(__name__)
@@ -10,7 +8,7 @@ app = Flask(__name__)
 TELEGRAM_API_TOKEN = os.environ['BOT_TOKEN']
 variables = {'fromAddress' : '\nfrom: ', 'toAddress' : '\nto: ', 'value' : '\nsent: ', 'asset' : ' ', 'external' : '\ncategory: ', 'hash': '\nhash: '}
 user_chat_id = os.environ['CHANNEL_ID']
-user_id = os.environ['USER_ID']
+
 
 @app.route('/')
 def hello():
@@ -27,14 +25,10 @@ def notify():
         res = logs['event']['activity'][0].get(i)
         if res: message += f"<b>{variables[i]}</b>" + f"<code>{str(res)}</code>"
     
-
+    message += f"\n\n{logs}"
 
     url = f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage?chat_id={user_chat_id}&parse_mode=HTML&text={message}"
     requests.get(url, stream=True)
-    
-    debug = "https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage?chat_id={user_id}&text={logs}"       
-    requests.get(debug, stream=True)
-    
     return Response(status=200)
   
   
