849665a8939e76148a055de00a31c3e74e7e78f6
doonay333331 <doonay333331@gmail.com>
2023-05-24 15:44:50 +0300

    1.8

--- app.py
+++ app.py
@@ -14,9 +14,18 @@ NETWORK = {
     "ETH_MAINNET": {"name" : "ETH", "url_address" : "https://etherscan.io/address/", "url_hash" : "https://etherscan.io/tx/"}, 
     "OPT_MAINNET": {"name" : "OPTIMISM", "url_address" : "https://optimistic.etherscan.io/address/", "url_hash" : "https://optimistic.etherscan.io/tx/"}}
 
+NAMES = {}
+
 @app.route('/')
 def hello():
     return 'Service for sending notifications to a telegram channel '
+
+
+@app.route('/names', methods=['POST'])
+def names():
+    global NAMES
+    NAMES = request.json
+    return Response(status=200)
 
 
 @app.route('/notify', methods=['POST','GET'])
@@ -25,13 +34,24 @@ def notify():
     
     message = ""
     k = logs['event'].get('network')
-    if k: message += f"<b>{NETWORK[k]['name']}</b>"
+
+    from_name = NAMES.get(logs['event']['activity'][0].get('fromAddress'))
+    to_name = NAMES.get(logs['event']['activity'][0].get('toAddress'))
+    if from_name:
+        wallet_name = f"<code>{from_name}</code>"
+    elif to_name:
+        wallet_name = f"<code>{to_name}</code>"
+    else:
+        wallet_name = "None"
+
+
+    if k: message += f"{wallet_name} · <b>{NETWORK[k]['name']}</b>"
     for i in variables:
         res = logs['event']['activity'][0].get(i)
         if res and (i == "fromAddress" or i == "toAddress"): 
             message += f"<i>{variables[i]}</i> <a href='{NETWORK[k]['url_address'] + str(res)}'>{str(res)[:6]}..{str(res)[-4:]}</a>"
         elif res and i == "hash":
-            message += f"\n<a href='{NETWORK[k]['url_hash'] + str(res)}'>Tx hash</a>"
+            message += f"\n · <a href='{NETWORK[k]['url_hash'] + str(res)}'>Tx hash</a>"
         elif res:
             message += f"<i>{variables[i]}</i>" + f"<code>{str(res)}</code>"
     
